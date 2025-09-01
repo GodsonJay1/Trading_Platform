bb34b71b541fff2d086ee54f906438b99d7ba9bf
GodsonJay1 <144109888+GodsonJay1@users.noreply.github.com>
2025-09-01 21:33:48 +0100

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:5455"
+export const API_BASE_URL = "http://192.168.56.12:5455"
 
 const api=axios.create({
     baseURL:API_BASE_URL,
