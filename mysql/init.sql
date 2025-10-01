ebd8d4c8ed205180bed3fa2e6bbf0421fe249234
Godsonjay1 <chinurugodson25@gmail.com>
2025-10-01 09:34:10 +0100

    corrected docker-compose

---
+++ mysql/init.sql
@@ -0,0 +1,6 @@
+-- mysql/init.sql
+CREATE DATABASE IF NOT EXISTS trust_trading;
+USE trust_trading;
+
+-- create a demo user/table if required
+-- CREATE TABLE IF NOT EXISTS sample (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100));
