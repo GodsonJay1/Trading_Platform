422f10841540c5f1e8f766b6b128aee8df769708
Godsonjay1 <chinurugodson25@gmail.com>
2025-11-13 07:36:29 +0100

    updated gemini api-key

--- backend/trading/src/main/java/com/jgc/trading/service/ChatbotServiceImpl.java
+++ backend/trading/src/main/java/com/jgc/trading/service/ChatbotServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Service
 public class ChatbotServiceImpl implements ChatbotService {
 
-    String GEMINI_API_KEY="AIzaSyDdtZqhF5JhhgCtew5rLH8BLOXqbP1N5LU";
+    String GEMINI_API_KEY="AIzaSyAzkEHKGnHflxz2fJu-PVslYSybju_bW9c";
 
     private double convertToDouble(Object value){
         if (value instanceof Integer){
